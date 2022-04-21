b5026804e0772d37e2dda53069f17eda50eb98df
madvier83 <madvier83@gmail.com>
2022-04-22 02:08:21 +0700

    Looping object and bear...

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ const vm = new Vue({
         message: 'hello world',
         img: '01.png',
         menu: 'test1',
+        bear: 'Go on a walk?',
         qty: 1,
         bil: 3,
         testArray: ['test1', 'test2', 'test3'],
         numbers: [1,2,3,4,5],
-        materi: ['Vue.js']
+        materi: ['Vue.js'],
+        book: {
+            title: 'Metamorphosis (177013)',
+            author: 'Shindo L'
+        }
     },
     methods:{
         getName: function(){
@@ -22,6 +27,20 @@ const vm = new Vue({
             return numbers.filter(data => {
                 return data % 2 == 0;
             })
+        },
+        greet: function(name){
+            return alert('Hello '+name+'!');
+        },
+        dangerLevel: function(number){
+            if(number>0.9){
+                return 'red';
+            }
+            if(number>0.5&&number<0.9){
+                return 'orange';
+            }
+            if(number>0&&number<0.5){
+                return 'green';
+            }
         }
     },
     computed: {
